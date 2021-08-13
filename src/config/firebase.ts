96ca11912e2b4b9d38a94007d8a3e92191aef166
andrew kariuki <andrew4kariuki@gmail.com>
2021-08-13 16:07:15 +0300

    chore(wip): work in progress

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,11 +1,9 @@
-/* eslint-disable prettier/prettier */
 export const firebaseConfig = {
-  apiKey: 'AIzaSyDTdqtuZUY9Nd8Mgk0Vdk87I7m5FBHw_Ow',
-  authDomain: 'fir-rn-7fe7c.firebaseapp.com',
-  databaseURL: 'https://fir-rn-7fe7c-default-rtdb.firebaseio.com',
-  projectId: 'fir-rn-7fe7c',
-  storageBucket: 'fir-rn-7fe7c.appspot.com',
-  messagingSenderId: '167327679668',
-  appId: '1:167327679668:web:b15b0cc19744edcfd483e1',
-  measurementId: 'G-8DPKKPNEYG',
+  apiKey: "AIzaSyDwi9qm9RZJOO-QW1n0YZjW4dwtdSAB9Tw",
+  authDomain: "wehv-d8f2a.firebaseapp.com",
+  projectId: "wehv-d8f2a",
+  storageBucket: "wehv-d8f2a.appspot.com",
+  messagingSenderId: "817836723386",
+  appId: "1:817836723386:web:b3208e93990431efb40683",
+  measurementId: "G-6K7XGR4SCJ",
 };
---
+++ src/stores/index.ts
@@ -0,0 +1,3 @@
+import { registerUser } from "./services/auth/register";
+
+export { registerUser };
